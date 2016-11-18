0ec8c8b8e31cf33f572f8349654afcc52c58ccfb
Antoine Gersant <antoine.gersant@lesforges.org>
2016-11-17 22:52:26 -0800

    Moved activities to separate package

--- app/src/main/java/agersant/polaris/activity/CollectionActivity.java
+++ app/src/main/java/agersant/polaris/activity/CollectionActivity.java
@@ -1,4 +1,4 @@
-package agersant.polaris;
+package agersant.polaris.activity;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +8,8 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Button;
+
+import agersant.polaris.R;
 
 public class CollectionActivity extends AppCompatActivity {
 
--- app/src/main/java/agersant/polaris/activity/SettingsActivity.java
+++ app/src/main/java/agersant/polaris/activity/SettingsActivity.java
@@ -1,4 +1,4 @@
-package agersant.polaris;
+package agersant.polaris.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
