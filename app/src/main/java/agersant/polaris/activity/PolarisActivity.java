1f6a56d47ab7c93b9efde1a6db6e0b368fa93d50
Antoine Gersant <antoine.gersant@lesforges.org>
2016-11-19 00:18:35 -0800

    Missing file from previous commit

--- app/src/main/java/agersant/polaris/activity/PolarisActivity.java
+++ app/src/main/java/agersant/polaris/activity/PolarisActivity.java
@@ -3,6 +3,8 @@ package agersant.polaris.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -10,6 +12,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import agersant.polaris.R;
+import agersant.polaris.activity.queue.QueueActivity;
 
 public class PolarisActivity extends AppCompatActivity {
 
@@ -26,6 +29,39 @@ public class PolarisActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(title);
         setSupportActionBar(toolbar);
+
+        final PolarisActivity that = this;
+        BottomNavigationView navigationView = (BottomNavigationView) findViewById(R.id.navigation);
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(final @NonNull MenuItem menuItem) {
+                return that.onNavigationItemSelected(menuItem);
+            }
+        });
+    }
+
+    private boolean onNavigationItemSelected(final MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_collection:
+                openCollection();
+                return true;
+            case R.id.nav_queue:
+                openQueue();
+                return true;
+        }
+        return false;
+    }
+
+    private void openCollection() {
+        Intent intent = new Intent(this, CollectionActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        startActivity(intent);
+    }
+
+    private void openQueue() {
+        Intent intent = new Intent(this, QueueActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        startActivity(intent);
     }
 
     @Override
@@ -35,6 +71,7 @@ public class PolarisActivity extends AppCompatActivity {
         return true;
     }
 
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_settings:
