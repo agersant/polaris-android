953908402c3bd87f273e0c48ae3fc4fc63b54211
Antoine Gersant <antoine.gersant@lesforges.org>
2016-11-18 17:48:19 -0800

    Import cleanup

--- app/src/main/java/agersant/polaris/activity/SettingsActivity.java
+++ app/src/main/java/agersant/polaris/activity/SettingsActivity.java
@@ -1,7 +1,7 @@
 package agersant.polaris.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import agersant.polaris.fragment.SettingsFragment;
 
