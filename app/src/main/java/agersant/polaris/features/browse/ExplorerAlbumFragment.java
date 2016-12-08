21702f5bc1fa7ce30aee1f9d1312aec1d9bcdacf
Antoine Gersant <antoine.gersant@lesforges.org>
2016-12-07 23:12:00 -0800

    Display album art, title and artist in explorer album view

--- app/src/main/java/agersant/polaris/features/browse/ExplorerAlbumFragment.java
+++ app/src/main/java/agersant/polaris/features/browse/ExplorerAlbumFragment.java
@@ -8,16 +8,23 @@ import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 import agersant.polaris.CollectionItem;
 import agersant.polaris.R;
+import agersant.polaris.api.ServerAPI;
+import agersant.polaris.ui.NetworkImage;
 
 
 public class ExplorerAlbumFragment extends ExplorerFragment {
 
     private ExplorerAdapter adapter;
+    private ImageView artwork;
+    private TextView artist;
+    private TextView title;
     private ArrayList<CollectionItem> items;
 
     public ExplorerAlbumFragment() {
@@ -27,6 +34,9 @@ public class ExplorerAlbumFragment extends ExplorerFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_explorer_album, container, false);
+        artwork = (ImageView) view.findViewById(R.id.album_artwork);
+        artist = (TextView) view.findViewById(R.id.album_artist);
+        title = (TextView) view.findViewById(R.id.album_title);
 
         RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.browse_recycler_view);
         recyclerView.setHasFixedSize(true);
@@ -38,9 +48,8 @@ public class ExplorerAlbumFragment extends ExplorerFragment {
 
         adapter = new ExplorerAdapter();
         recyclerView.setAdapter(adapter);
-        if (items != null) {
-            adapter.setItems(items);
-        }
+
+        populate();
 
         return view;
     }
@@ -49,12 +58,40 @@ public class ExplorerAlbumFragment extends ExplorerFragment {
     public void onDestroyView() {
         super.onDestroyView();
         adapter = null;
+        artwork = null;
+        artist = null;
+        title = null;
     }
 
     public void setItems(ArrayList<CollectionItem> items) {
+        assert !items.isEmpty();
         this.items = items;
-        if (adapter != null) {
-            adapter.setItems(items);
+        populate();
+    }
+
+    private void populate() {
+        if (items == null || adapter == null) {
+            return;
+        }
+
+        adapter.setItems(items);
+
+        CollectionItem item = items.get(0);
+        String artworkPath = item.getArtwork();
+        if (artworkPath != null) {
+            ServerAPI serverAPI = ServerAPI.getInstance(getActivity());
+            String url = serverAPI.getMediaURL(artworkPath);
+            NetworkImage.load(url, artwork);
+        }
+
+        String titleString = item.getTitle();
+        if (title != null) {
+            title.setText(titleString);
+        }
+
+        String artistString = item.getArtist();
+        if (artist != null) {
+            artist.setText(artistString);
         }
     }
 
