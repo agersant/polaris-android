8f2fe6ea19747424dfeb8b332f60630fe66e77a8
Antoine Gersant <antoine.gersant@lesforges.org>
2016-12-21 08:36:24 +0100

    Added landscape layout for player

--- app/src/main/java/agersant/polaris/ui/SquareLayout.java
+++ app/src/main/java/agersant/polaris/ui/SquareLayout.java
@@ -1,10 +1,15 @@
 package agersant.polaris.ui;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.widget.FrameLayout;
 
+import agersant.polaris.R;
+
 public class SquareLayout extends FrameLayout {
+
+	private boolean preserveWidth = true;
 
 	public SquareLayout(Context context) {
 		super(context);
@@ -12,22 +17,44 @@ public class SquareLayout extends FrameLayout {
 
 	public SquareLayout(Context context, AttributeSet attrs) {
 		super(context, attrs);
+		readAttributes(context, attrs);
 	}
 
 	public SquareLayout(Context context, AttributeSet attrs, int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
+		readAttributes(context, attrs);
 	}
 
 	public SquareLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
 		super(context, attrs, defStyleAttr, defStyleRes);
+		readAttributes(context, attrs);
+	}
+
+	private void readAttributes(Context context, AttributeSet attrs) {
+		TypedArray arr = context.obtainStyledAttributes(attrs, R.styleable.SquareLayout);
+		boolean preserveWidthArg = arr.getBoolean(R.styleable.SquareLayout_preserveWidth, true);
+		setPreserveWidth(preserveWidthArg);
+		arr.recycle();
 	}
 
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-		int squareSize = getMeasuredWidth();
+		int squareSize;
+		if (preserveWidth) {
+			squareSize = getMeasuredWidth();
+		} else {
+			squareSize = getMeasuredHeight();
+		}
 		int measureSpec = MeasureSpec.makeMeasureSpec(squareSize, MeasureSpec.EXACTLY);
 		super.onMeasure(measureSpec, measureSpec);
 	}
 
+	public boolean isPreserveWidth() {
+		return preserveWidth;
+	}
+
+	public void setPreserveWidth(boolean preserveWidth) {
+		this.preserveWidth = preserveWidth;
+	}
 }
