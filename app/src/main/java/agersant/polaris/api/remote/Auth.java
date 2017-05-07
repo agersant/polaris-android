7656059a398adb3008bd32de7f064373c6b86732
Antoine Gersant <antoine.gersant@lesforges.org>
2017-05-07 16:22:27 -0700

    Fixed network errors when using incorrect credentials

--- app/src/main/java/agersant/polaris/api/remote/Auth.java
+++ app/src/main/java/agersant/polaris/api/remote/Auth.java
@@ -3,13 +3,19 @@ package agersant.polaris.api.remote;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.util.Base64;
 
+import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import agersant.polaris.R;
+import okhttp3.Authenticator;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.Route;
 
-class Auth {
+class Auth implements Authenticator {
 
 	private static final Pattern setCookiePattern = Pattern.compile("^(.*);");
 	private SharedPreferences preferences;
@@ -19,14 +25,10 @@ class Auth {
 	private String passwordKey;
 
 	Auth(Context context) {
-		this.preferences = PreferenceManager.getDefaultSharedPreferences(context);
+		preferences = PreferenceManager.getDefaultSharedPreferences(context);
 		usernameKey = context.getString(R.string.pref_key_username);
 		passwordKey = context.getString(R.string.pref_key_password);
-		this.cookie = null;
-	}
-
-	String getCookie() {
-		return this.cookie;
+		cookie = null;
 	}
 
 	void parseCookie(String header) {
@@ -36,9 +38,32 @@ class Auth {
 		}
 	}
 
-	String getAuthorizationHeader() {
+	private String getAuthorizationHeader() {
 		String username = preferences.getString(usernameKey, "");
 		String password = preferences.getString(passwordKey, "");
 		return username + ":" + password;
+	}
+
+	@Override
+	public Request authenticate(Route route, Response response) throws IOException {
+		Request.Builder newRequest = response.request().newBuilder();
+
+		String oldCookie = response.request().header("Cookie");
+		boolean newCookie = cookie != null && (oldCookie == null || !cookie.equals(oldCookie));
+		if (newCookie) {
+			newRequest.header("Cookie", cookie);
+			return newRequest.build();
+		}
+
+		String authorization = getAuthorizationHeader();
+		authorization = "Basic " + Base64.encodeToString(authorization.getBytes(), Base64.NO_WRAP);
+		String oldAuthorization = response.request().header("Authorization");
+		boolean newAuthorization = oldAuthorization == null || !authorization.equals(oldAuthorization);
+		if (newAuthorization) {
+			newRequest.header("Authorization", authorization);
+			return newRequest.build();
+		}
+
+		return null;
 	}
 }
--- app/src/main/java/agersant/polaris/api/remote/RequestQueue.java
+++ app/src/main/java/agersant/polaris/api/remote/RequestQueue.java
@@ -1,18 +1,15 @@
 package agersant.polaris.api.remote;
 
 import android.content.Context;
-import android.util.Base64;
 
 import java.io.IOException;
 
-import okhttp3.Authenticator;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
-import okhttp3.Route;
 
 public class RequestQueue {
 
@@ -24,21 +21,7 @@ public class RequestQueue {
 		this.auth = new Auth(context);
 		this.client = new OkHttpClient.Builder()
 				.retryOnConnectionFailure(true)
-				.authenticator(new Authenticator() {
-					@Override
-					public Request authenticate(Route route, Response response) throws IOException {
-						Request.Builder newRequest = response.request().newBuilder();
-						String authCookie = auth.getCookie();
-						if (authCookie != null) {
-							newRequest.header("Cookie", authCookie);
-						} else {
-							String authorization = auth.getAuthorizationHeader();
-							authorization = "Basic " + Base64.encodeToString(authorization.getBytes(), Base64.NO_WRAP);
-							newRequest.header("Authorization", authorization);
-						}
-						return newRequest.build();
-					}
-				})
+				.authenticator(auth)
 				.build();
 	}
 
@@ -73,11 +56,13 @@ public class RequestQueue {
 
 			@Override
 			public void onResponse(Call call, Response response) throws IOException {
-				if (auth.getCookie() == null) {
-					String setCookie = response.header("Set-Cookie", null);
-					if (setCookie != null) {
-						auth.parseCookie(setCookie);
-					}
+				if (response.code() != 200) {
+					callback.onFailure(call, null);
+					return;
+				}
+				String setCookie = response.header("Set-Cookie", null);
+				if (setCookie != null) {
+					auth.parseCookie(setCookie);
 				}
 				callback.onResponse(call, response);
 			}
