9f1ace6745e280a8f2247a0fcb9ff35d074700c7
Antoine Gersant <antoine.gersant@lesforges.org>
2020-12-23 00:20:33 -0800

    Extract connection error handling to mixing

--- lib/ui/startup/connect.dart
+++ lib/ui/startup/connect.dart
@@ -12,42 +12,8 @@ class ConnectForm extends StatefulWidget {
   _ConnectFormState createState() => _ConnectFormState();
 }
 
-class _ConnectFormState extends State<ConnectForm> {
+class _ConnectFormState extends State<ConnectForm> with ConnectionErrorHandler {
   final _textEditingController = TextEditingController();
-  StreamSubscription<ConnectionStoreError> _connectionErrorStream;
-
-  @override
-  void initState() {
-    super.initState();
-    _connectionErrorStream = getIt<ConnectionStore>().errorStream.listen((e) {
-      Scaffold.of(context).removeCurrentSnackBar();
-      switch (e) {
-        case ConnectionStoreError.unsupportedAPIVersion:
-          Scaffold.of(context).showSnackBar(SnackBar(
-              content: Text(
-                  "The Polaris server responded but uses an incompatible API version.")));
-          break;
-        case ConnectionStoreError.connectionAlreadyInProgress:
-          Scaffold.of(context).showSnackBar(SnackBar(
-              content: Text(
-                  "Please wait while the connection is being established.")));
-          break;
-        case ConnectionStoreError.networkError:
-          Scaffold.of(context).showSnackBar(SnackBar(
-              content: Text("The Polaris server could not be reached.")));
-          break;
-        case ConnectionStoreError.requestFailed:
-          Scaffold.of(context).showSnackBar(SnackBar(
-              content:
-                  Text("The Polaris server sent an unexpected response.")));
-          break;
-        case ConnectionStoreError.unknownError:
-          Scaffold.of(context).showSnackBar(
-              SnackBar(content: Text("An unknown error occured.")));
-          break;
-      }
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -84,6 +50,49 @@ class _ConnectFormState extends State<ConnectForm> {
   @override
   void dispose() {
     _textEditingController.dispose();
+    super.dispose();
+  }
+}
+
+mixin ConnectionErrorHandler<T extends StatefulWidget> on State<T> {
+  StreamSubscription<ConnectionStoreError> _connectionErrorStream;
+
+  @override
+  void initState() {
+    super.initState();
+    _connectionErrorStream =
+        getIt<ConnectionStore>().errorStream.listen((e) => handleError(e));
+  }
+
+  void handleError(ConnectionStoreError error) {
+    Scaffold.of(context).removeCurrentSnackBar();
+    switch (error) {
+      case ConnectionStoreError.unsupportedAPIVersion:
+        Scaffold.of(context).showSnackBar(SnackBar(
+            content: Text(
+                "The Polaris server responded but uses an incompatible API version.")));
+        break;
+      case ConnectionStoreError.connectionAlreadyInProgress:
+        Scaffold.of(context).showSnackBar(SnackBar(
+            content: Text(
+                "Please wait while the connection is being established.")));
+        break;
+      case ConnectionStoreError.networkError:
+        Scaffold.of(context).showSnackBar(SnackBar(
+            content: Text("The Polaris server could not be reached.")));
+        break;
+      case ConnectionStoreError.requestFailed:
+        Scaffold.of(context).showSnackBar(SnackBar(
+            content: Text("The Polaris server sent an unexpected response.")));
+        break;
+      case ConnectionStoreError.unknownError:
+        Scaffold.of(context)
+            .showSnackBar(SnackBar(content: Text("An unknown error occured.")));
+        break;
+    }
+  }
+
+  void dispose() {
     _connectionErrorStream.cancel();
     super.dispose();
   }
