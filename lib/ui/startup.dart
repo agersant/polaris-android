20425623e742124620a029987f54780f86b20dd8
Antoine Gersant <antoine.gersant@lesforges.org>
2020-12-22 16:42:04 -0800

    Landscape mode support

--- lib/ui/startup.dart
+++ lib/ui/startup.dart
@@ -9,11 +9,8 @@ final getIt = GetIt.instance;
 class StartupPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Widget logo = FractionallySizedBox(
-      widthFactor: 0.5,
-      child: SvgPicture.asset('assets/images/logo.svg',
-          alignment: Alignment.bottomCenter, semanticsLabel: 'Polaris logo'),
-    );
+    Widget logo = SvgPicture.asset('assets/images/logo.svg',
+        semanticsLabel: 'Polaris logo');
 
     Widget serverForm = Form(
       child: Column(
@@ -55,37 +52,53 @@ class StartupPage extends StatelessWidget {
       ),
     );
 
-    return Scaffold(
-        body: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Expanded(
-          flex: 50,
-          child: logo,
-        ),
-        Expanded(
-          flex: 75,
-          child: Padding(
-            padding: const EdgeInsets.all(48),
-            child: Consumer<ConnectionStore>(
-              builder: (context, connection, child) {
-                switch (connection.state) {
-                  case ConnectionState.disconnected:
-                    return serverForm;
-                  case ConnectionState.connecting:
-                    return Align(
-                        alignment: Alignment.topCenter,
-                        child: CircularProgressIndicator());
-                  case ConnectionState.connected:
-                  default:
-                    return Text("we gucci");
-                }
-              },
+    return Scaffold(body: OrientationBuilder(builder: (context, orientation) {
+      EdgeInsets padding = orientation == Orientation.portrait
+          ? const EdgeInsets.symmetric(horizontal: 48.0)
+          : const EdgeInsets.all(0);
+      Axis direction =
+          orientation == Orientation.portrait ? Axis.vertical : Axis.horizontal;
+
+      return Padding(
+        padding: padding,
+        child: Flex(
+          direction: direction,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Spacer(
+              flex: 40,
             ),
-          ),
+            Expanded(flex: 50, child: logo),
+            Spacer(
+              flex: 20,
+            ),
+            Expanded(
+              flex: 100,
+              child: IntrinsicHeight(
+                child: Consumer<ConnectionStore>(
+                  builder: (context, connection, child) {
+                    switch (connection.state) {
+                      case ConnectionState.disconnected:
+                        return serverForm;
+                      case ConnectionState.connecting:
+                        return Align(
+                            alignment: Alignment.topCenter,
+                            child: CircularProgressIndicator());
+                      case ConnectionState.connected:
+                      default:
+                        return Text("we gucci");
+                    }
+                  },
+                ),
+              ),
+            ),
+            Spacer(
+              flex: 40,
+            ),
+          ],
         ),
-      ],
-    ));
+      );
+    }));
   }
 
   onConnectPressed() {
