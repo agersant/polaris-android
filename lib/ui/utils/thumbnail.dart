deb08e2e34b6dfc9e2600f35ee0a0ca289a3a43d
Antoine Gersant <antoine.gersant@lesforges.org>
2020-12-26 22:57:49 -0800

    Fixed thumbnails blinking when changing page

--- lib/ui/utils/thumbnail.dart
+++ lib/ui/utils/thumbnail.dart
@@ -4,23 +4,45 @@ import 'package:polaris/collection/interface.dart' as collection;
 
 final getIt = GetIt.instance;
 
-class Thumbnail extends StatelessWidget {
+class Thumbnail extends StatefulWidget {
   final String path;
 
   Thumbnail(this.path, {Key key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final interface = getIt<collection.Interface>();
-    final imageProvider = interface.getImage(path);
+  _ThumbnailState createState() => _ThumbnailState();
+}
 
+class _ThumbnailState extends State<Thumbnail> {
+  Future<ImageProvider> _imageProvider;
+
+  @override
+  void initState() {
+    super.initState();
+    final interface = getIt<collection.Interface>();
+    _imageProvider = interface.getImage(widget.path);
+  }
+
+  @override
+  void didUpdateWidget(Thumbnail oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    final interface = getIt<collection.Interface>();
+    if (oldWidget.path != widget.path) {
+      setState(() {
+        _imageProvider = interface.getImage(widget.path);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return FutureBuilder(
-      future: imageProvider,
+      future: _imageProvider,
       builder: (context, snapshot) {
         if (snapshot.connectionState != ConnectionState.done) {
           return Container();
         }
-        if (imageProvider == null || snapshot.hasError || snapshot.data == null) {
+        if (_imageProvider == null || snapshot.hasError || snapshot.data == null) {
           return Container(); // TODO stripes https://medium.com/@baobao1996mn/flutter-draw-striped-objects-with-custompainter-4955f5014706
         }
         assert(snapshot.hasData);
