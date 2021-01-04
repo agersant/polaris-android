10dbefe2aade101e0df8832c19aee7317db1f617
Antoine Gersant <antoine.gersant@lesforges.org>
2021-01-03 21:35:47 -0800

    Fixed MediaProxy error when playing files with non ASCII names

--- lib/playback/media_proxy.dart
+++ lib/playback/media_proxy.dart
@@ -29,12 +29,8 @@ class MediaProxy {
 
       request.response.contentLength = streamedResponse.contentLength ?? -1;
       request.response.statusCode = streamedResponse.statusCode;
-      for (var header in streamedResponse.headers.entries) {
-        // TODO explodes when path contains non-ASCII characters w/ content-disposition header
-        request.response.headers.add(header.key, header.value);
-      }
       request.response.headers
-          .removeAll(HttpHeaders.contentEncodingHeader); // Compression already handled, we have the raw response
+          .add(HttpHeaders.contentTypeHeader, streamedResponse.headers[HttpHeaders.contentTypeHeader]);
 
       streamedResponse.stream.pipe(request.response);
     });
