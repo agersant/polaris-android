a94922a4407a4658fc4395d1a2d836f6747f9b5c
Antoine Gersant <antoine.gersant@lesforges.org>
2021-01-05 00:03:11 -0800

    Added note

--- lib/transient/audio_service_widget.dart
+++ lib/transient/audio_service_widget.dart
@@ -3,6 +3,10 @@ import 'package:flutter/widgets.dart';
 
 // Copy-pasta from the widget with the same name from audio_service, but with handler for didPopRoute()
 // We don't want to disconnect when random popups get closed
+// Related to this hack. Warning when backing out of app:
+// Activity agersant.polaris.MainActivity has leaked ServiceConnection android.media.browse.MediaBrowser$MediaServiceConnection@12e1f74 that was originally bound here
+// https://github.com/ryanheise/audio_service/issues/73
+// https://github.com/ryanheise/audio_service/pull/77
 // TODO submit bug report about this
 class PolarisAudioServiceWidget extends StatefulWidget {
   final Widget child;
