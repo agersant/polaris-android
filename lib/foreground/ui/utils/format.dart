119be72f886b6c9e637c3a8c966c4e613d8927c3
Antoine Gersant <antoine.gersant@lesforges.org>
2021-01-09 15:57:06 -0800

    Fixed bad formatting

--- lib/foreground/ui/utils/format.dart
+++ lib/foreground/ui/utils/format.dart
@@ -7,9 +7,8 @@ String formatDuration(Duration d) {
   String twoDigits(int n) => n.toString().padLeft(2, '0');
   String twoDigitMinutes = twoDigits(d.inMinutes.remainder(60));
   String twoDigitSeconds = twoDigits(d.inSeconds.remainder(60));
-  // TODO broken for Juno Reactor Luciana
-  String hours = '$d.inHours';
   if (d.inHours > 0) {
+    String hours = '${d.inHours}';
     return '$hours:$twoDigitMinutes:$twoDigitSeconds';
   } else {
     return '$twoDigitMinutes:$twoDigitSeconds';
