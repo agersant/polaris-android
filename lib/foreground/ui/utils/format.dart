10ae4312836050755fa1e5ac770edfb1df81a396
Antoine Gersant <antoine.gersant@lesforges.org>
2021-01-09 15:30:43 -0800

    Added TODO

--- lib/foreground/ui/utils/format.dart
+++ lib/foreground/ui/utils/format.dart
@@ -7,6 +7,7 @@ String formatDuration(Duration d) {
   String twoDigits(int n) => n.toString().padLeft(2, '0');
   String twoDigitMinutes = twoDigits(d.inMinutes.remainder(60));
   String twoDigitSeconds = twoDigits(d.inSeconds.remainder(60));
+  // TODO broken for Juno Reactor Luciana
   String hours = '$d.inHours';
   if (d.inHours > 0) {
     return '$hours:$twoDigitMinutes:$twoDigitSeconds';
