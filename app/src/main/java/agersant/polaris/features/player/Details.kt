1d55d793b1fbef802f39fb15b9d71961484ae7e3
Saecki <tobiasschmitz2001@gmail.com>
2021-04-18 00:02:41 +0200

    create top level show details dialog function

---
+++ app/src/main/java/agersant/polaris/features/player/Details.kt
@@ -0,0 +1,50 @@
+package agersant.polaris.features.player
+
+import agersant.polaris.CollectionItem
+import agersant.polaris.R
+import agersant.polaris.databinding.ViewSongDetailsBinding
+import agersant.polaris.util.formatTime
+import android.content.Context
+import android.os.Handler
+import android.view.LayoutInflater
+import androidx.appcompat.app.AlertDialog
+import androidx.core.view.isVisible
+
+fun showDetailsDialog(context: Context, item: CollectionItem): AlertDialog = context.run {
+    val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+    val detailsBinding = ViewSongDetailsBinding.inflate(inflater).apply {
+        scrollView.setOnScrollChangeListener { v, _, _, _, _ ->
+            topDivider.isVisible = (v.canScrollVertically(-1))
+            bottomDivider.isVisible = (v.canScrollVertically(1))
+        }
+
+        path.text = item.path
+        albumArtist.text = item.albumArtist ?: context.getString(R.string.player_unknown)
+        artist.text = item.artist ?: getString(R.string.player_unknown)
+        album.text = item.album ?: getString(R.string.player_unknown)
+        title.text = item.title ?: getString(R.string.player_unknown)
+        discNumber.text = if (item.discNumber != -1) item.discNumber.toString() else getString(R.string.player_unknown)
+        trackNumber.text = if (item.trackNumber != -1) item.trackNumber.toString() else getString(R.string.player_unknown)
+        year.text = if (item.year != -1) item.year.toString() else getString(R.string.player_unknown)
+        duration.text = if (item.duration != -1) {
+            formatTime(item.duration)
+        } else {
+            getString(R.string.player_unknown)
+        }
+    }
+
+    val dialog = AlertDialog.Builder(context)
+        .setTitle(R.string.player_details)
+        .setView(detailsBinding.root)
+        .setPositiveButton(android.R.string.ok, null)
+        .create()
+
+    Handler().post {
+        detailsBinding.topDivider.isVisible = detailsBinding.scrollView.canScrollVertically(-1)
+        detailsBinding.bottomDivider.isVisible = detailsBinding.scrollView.canScrollVertically(1)
+    }
+
+    dialog.show()
+
+    return dialog
+}
--- app/src/main/java/agersant/polaris/features/player/PlayerFragment.kt
+++ app/src/main/java/agersant/polaris/features/player/PlayerFragment.kt
@@ -1,9 +1,11 @@
 package agersant.polaris.features.player
 
-import agersant.polaris.*
+import agersant.polaris.PlaybackQueue
+import agersant.polaris.PolarisApplication
+import agersant.polaris.PolarisPlayer
+import agersant.polaris.R
 import agersant.polaris.api.API
 import agersant.polaris.databinding.FragmentPlayerBinding
-import agersant.polaris.databinding.ViewPlayerDetailsBinding
 import agersant.polaris.util.formatTime
 import android.content.BroadcastReceiver
 import android.content.Context
@@ -16,7 +18,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.google.android.material.progressindicator.CircularProgressIndicator
@@ -225,29 +226,11 @@ class PlayerFragment : Fragment() {
 
     private fun showDetails() {
         val item = player.currentItem ?: return
-        val detailsBinding = ViewPlayerDetailsBinding.inflate(layoutInflater)
 
-        detailsBinding.path.text = item.path
-        detailsBinding.albumArtist.text = item.albumArtist ?: getString(R.string.player_unknown)
-        detailsBinding.artist.text = item.artist ?: getString(R.string.player_unknown)
-        detailsBinding.album.text = item.album ?: getString(R.string.player_unknown)
-        detailsBinding.title.text = item.title ?: getString(R.string.player_unknown)
-        detailsBinding.discNumber.text = if (item.discNumber != -1) item.discNumber.toString() else getString(R.string.player_unknown)
-        detailsBinding.trackNumber.text = if (item.trackNumber != -1) item.trackNumber.toString() else getString(R.string.player_unknown)
-        detailsBinding.year.text = if (item.year != -1) item.year.toString() else getString(R.string.player_unknown)
-        detailsBinding.duration.text = if (item.duration != -1) {
-            formatTime(item.duration)
-        } else {
-            getString(R.string.player_unknown)
+        val dialog = showDetailsDialog(requireContext(), item)
+        dialog.setOnDismissListener {
+            viewModel.detailsShowing = false
         }
-
-        AlertDialog.Builder(requireContext())
-            .setTitle(R.string.player_details)
-            .setView(detailsBinding.root)
-            .setPositiveButton(android.R.string.ok, null)
-            .setOnDismissListener { viewModel.detailsShowing = false }
-            .show()
-
         viewModel.detailsShowing = true
     }
 }
