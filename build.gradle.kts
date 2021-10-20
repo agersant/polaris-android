22866778f45d3d39a847579b66e32ca5d3230269
Tobias Schmitz <tobiasschmitz2001@gmail.com>
2021-10-20 04:38:31 +0200

    Migrate to gradle kotlin dsl (#42)
    * migrate gradle to kotlin dsl
    
    * define dependencies in buildSrc
    
    * remove deprecated and unnecessary dependency declaration
    
    * rename Dependencies to Deps
    
    * add gradle check dependency updates plugin
    
    * update depenedencies
    
    * reorganize navigation dependency
    
    * remove unused repositories

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.s
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath(Deps.androidGradlePlugin)
+        classpath(Deps.Kotlin.gradlePlugin)
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle.kts files
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
